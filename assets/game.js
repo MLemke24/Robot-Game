e16c93d563b5443b72401bcf7a6e2c032fe8b5d8
Matt Lemke <mattrlemke@gmail.com>
2021-02-09 21:27:59 -0600

    refactored project

--- assets/game.js
+++ assets/game.js
@@ -1,71 +1,74 @@
-var fight = function() {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-}
+var playerName = window.prompt("What is your robot name?");
+var playerHealth = 100;
+var playerAttack = 10;\
+var playerMoney = 10;
 
-fight ();
-
-//My Robot
-var playerName = window.prompt ("What is your robot's name?");
- var playerHealth = 100;
- var playerAttack  = 10;
- var playerMoney = 10;
 console.log(playerName, playerAttack, playerHealth);
-
-//Enemy Robot 
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
-console.log(enemyName, enemyHealth, enemyAttack);
 
 
-    
-    // Would You like to attack?
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+//Fight Function
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
 
-    // if player choses to fight, then fight
+  // Fight or Skip
+
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose. ");
+
 if (promptFight === "fight" || promptFight === "FIGHT") {
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
-  
-    // check enemy's health
-    if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
-    } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
-  
-    // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
-  
-    // check player's health
-    if (playerHealth <= 0) {
-      window.alert(playerName + " has died!");
-    } else {
-      window.alert(playerName + " still has " + playerHealth + " health left.");
-    }
-   
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-  
-    // if yes (true), leave fight
-    if (confirmSkip) {
-      window.alert(playerName + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 2;
-    }
-    // if no (false), ask question again by running fight() again
-    else {
-      fight();
-    }
-  }
-    
 
+//Enemy Health after damange
+
+enemyHealth = enemyHealth - playerAttack;
+
+// Enemy health console log 
+
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining." 
+  );
+
+  // check enemies health 
+
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died! ");
+  }
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+  }
+
+  // Player Health after damage 
+
+  playerHealth = playerHealth - enemyAttack;
+
+  // Player health console log
+
+  console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+);
+
+// check player health
+
+if (playerHealth <=0) {
+  window.alert(playerName + " has died! ");
+}
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left. "); 
+}
+
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!" );
+    playerMoney = playerMoney - 2;
+  } else {
+    fight();
+  }
+} else {
+  window.alert("You need to choose a valid option. Try Again!");
+}
+};
+
+fight();
